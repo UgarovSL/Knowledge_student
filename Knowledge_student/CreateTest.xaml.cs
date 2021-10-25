ceda7cd7dda13a7bfcaf3dce110c96ecf4ee7ba7
UgarovSL <ugarovjob@vk.com>
2021-10-25 20:28:01 +0300

    Создание теста

--- Knowledge_student/CreateTest.xaml.cs
+++ Knowledge_student/CreateTest.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Knowledge_student
 {
@@ -20,6 +21,9 @@ namespace Knowledge_student
     /// </summary>
     public partial class CreateTest : Page
     {
+        const int minLengthName = 1;
+        const int maxLenghtName = 50;
+        const int maxMaxPoint = 4;
         public CreateTest()
         {
             InitializeComponent();
@@ -42,5 +46,78 @@ namespace Knowledge_student
             Uri tableTeatcher = new Uri("Table_Teatcher.xaml", UriKind.Relative);
             this.NavigationService.Navigate(tableTeatcher);
         }
+
+        private void btnCreateTests(object sender, RoutedEventArgs e)
+        {
+            string disNumber = textBoxNumberDiscipline.Text.Trim().ToLower();
+            string numberTheme = textBoxNumberTheme.Text.Trim();
+            string nameTests = textBoxNameTest.Text.Trim();
+            string maxPoint = textBoxMaxPoint.Text.Trim().ToLower();
+            string numberTeacher = textBoxNumberTeacher.Text.Trim().ToLower();
+
+            if (!Regex.IsMatch(disNumber, @"[\d0-9]"))
+            {
+
+                textBoxNumberDiscipline.ToolTip = "Некорректно введен номер дисциплины.";
+                var backgroundColor = new BrushConverter();
+                textBoxNumberDiscipline.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                textBoxNumberDiscipline.Background = Brushes.Transparent;
+                textBoxNumberDiscipline.ToolTip = null;
+            }
+
+            if ( !Regex.IsMatch(disNumber, @"[\d0-9]"))
+            {
+
+                textBoxNumberTheme.ToolTip = "Некорректно введен номер темы.";
+                var backgroundColor = new BrushConverter();
+                textBoxNumberTheme.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                textBoxNumberTheme.Background = Brushes.Transparent;
+                textBoxNumberTheme.ToolTip = null;
+            }
+            if (nameTests.Length >=maxLenghtName || nameTests.Length <= minLengthName || !Regex.IsMatch(nameTests, @"[\dа-я]"))
+            {
+
+                textBoxNameTest.ToolTip = "Некорректно введено название теста.";
+                var backgroundColor = new BrushConverter();
+                textBoxNameTest.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                textBoxNameTest.Background = Brushes.Transparent;
+                textBoxNameTest.ToolTip = null;
+            }
+
+            if (maxPoint.Length >= maxMaxPoint || !Regex.IsMatch(maxPoint, @"[\d0-9]"))
+            {
+
+                textBoxMaxPoint.ToolTip = "Некорректно введено кол-во баллов.";
+                var backgroundColor = new BrushConverter();
+                textBoxMaxPoint.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                textBoxMaxPoint.Background = Brushes.Transparent;
+                textBoxMaxPoint.ToolTip = null;
+            }
+            if (!Regex.IsMatch(numberTeacher, @"[\d0-9]"))
+            {
+
+                textBoxNumberTeacher.ToolTip = "Некорректно введен номер преподавателя.";
+                var backgroundColor = new BrushConverter();
+                textBoxNumberTeacher.Background = (Brush)backgroundColor.ConvertFrom("#FFFF5E5B");
+            }
+            else
+            {
+                textBoxNumberTeacher.Background = Brushes.Transparent;
+                textBoxNumberTeacher.ToolTip = null;
+
+
+
+            }
     }
-}
